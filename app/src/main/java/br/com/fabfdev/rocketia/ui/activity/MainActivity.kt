847a475855bf41b9003fd0ba38786f09e69ab124
Felipe Faustini <felipefaustini@MacBook-Pro-2.local>
2026-06-05 19:21:14 -0300

    feat: loading main activity and added packages for ui

--- app/src/main/java/br/com/fabfdev/rocketia/ui/activity/MainActivity.kt
+++ app/src/main/java/br/com/fabfdev/rocketia/ui/activity/MainActivity.kt
@@ -1,4 +1,4 @@
-package br.com.fabfdev.rocketia.ui
+package br.com.fabfdev.rocketia.ui.activity
 
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
@@ -6,16 +6,23 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import br.com.fabfdev.rocketia.R
+import br.com.fabfdev.rocketia.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.clMainContainer)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
     }
+
 }
